89c7dfbd2d637f45bb3719203a73198983e88b4f
filipkosec <filipkosec@outlook.com>
2022-10-23 11:16:20 +0200

    Add all the necessary logic, correct the git error.

--- Scripts.js
+++ Scripts.js
@@ -4,18 +4,29 @@ const buttonDecimal = document.querySelectorAll('.decimal')[0];
 const buttonClear = document.querySelectorAll('.clear')[0];
 const buttonDelete = document.querySelectorAll('.delete')[0];
 const valueDisplay = document.querySelectorAll('.screen')[0];
+const buttonMakeNegative = document.querySelectorAll(".makeNegative")[0];
+
 let screenValue = 0;
 let memoryValue = 0;
 let workingValue = "";
 let numOfDecimals = 0;
-valueDisplay.textContent = screenValue;
+let operatorChosen = false;
+let previousOperator = "";
+let chosenOperator = "";
 
+valueDisplay.textContent = screenValue;
 numButtons.forEach(numButton => numButton.addEventListener('click', () => numberAction(numButton)))
 operatorButtons.forEach(operatorButton => operatorButton.addEventListener('click', () => operatorAction(operatorButton)));
+buttonDelete.addEventListener('click', () => deleteAction());
+buttonClear.addEventListener('click', () => clearAction());
+buttonMakeNegative.addEventListener('click', () => makeNegative());
 
 function numberAction(numButton) {
+    if (operatorChosen === true) {
+        workingValue = "";
+        operatorChosen === false;
+    }
     workingValue += numButton.textContent;
-    console.log(numButton.textContent)
     if (numButton.textContent === "." && numOfDecimals>0) {
         alert("Only one decimal point!");
         workingValue = workingValue.slice(0,-1);
@@ -25,23 +36,61 @@ function numberAction(numButton) {
     valueDisplay.textContent = screenValue;
 }
 
-function operatorAction(operatorButton) {
-    workingValue = parseInt(workingValue);
-    let whichOperator = operatorButton.textContent;
-    console.log(whichOperator);
-    if (whichOperator === "+") {
-        memoryValue += workingValue;
-    } else if (whichOperator === "-") {
-        memoryValue -= workingValue
-    } else if (whichOperator === "*") {
-        memoryValue *= workingValue
-    } else if (whichOperator === "/") {
-        memoryValue /= workingValue
-    } else {
-        memoryValue += workingValue
-    };
-    screenValue = memoryValue;
+function deleteAction() {
+    if (workingValue.length === 1 || workingValue.length === 0) {workingValue = "00"};
+    workingValue = workingValue.slice(0,-1);
+    screenValue = workingValue;
     valueDisplay.textContent = screenValue;
+    if (screenValue === "0") {workingValue = ""};
+}
+
+function clearAction() {
+    screenValue = 0;
     memoryValue = 0;
     workingValue = "";
+    numOfDecimals = 0;
+    operatorChosen = false;
+    previousOperator = "";
+    chosenOperator = "";
+    valueDisplay.textContent = screenValue;
+}
+
+function makeNegative() {
+    if (workingValue.charAt(0) != "-") {
+        workingValue = "-" + workingValue;
+    } else {
+        workingValue = workingValue.substring(1);
+    }
+    screenValue = workingValue;
+    valueDisplay.textContent = screenValue;
+}
+
+function operatorAction(operatorButton) {
+    previousOperator = chosenOperator;
+    chosenOperator = operatorButton.textContent;
+    if (previousOperator != "") {
+        if (previousOperator === "+") {
+            memoryValue += parseFloat(workingValue);
+        } else if (previousOperator === "-") {
+            memoryValue -= parseFloat(workingValue);
+        } else if (previousOperator === "*") {
+            memoryValue *= parseFloat(workingValue);
+        } else if (previousOperator === "/") {
+            if (workingValue === "0") {
+                clearAction();
+                valueDisplay.textContent = "∞";
+                return;
+            }
+            memoryValue /= parseFloat(workingValue);
+        } else if (previousOperator === "=" && chosenOperator === "=") {
+            workingValue = screenValue;
+            memoryValue = screenValue;
+        }
+        screenValue = memoryValue;
+        valueDisplay.textContent = screenValue;
+    }
+    if (previousOperator === "") {
+        memoryValue = parseFloat(workingValue);
+    }
+    operatorChosen = true;
 }
